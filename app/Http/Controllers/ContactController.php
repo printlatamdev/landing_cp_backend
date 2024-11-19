6ec45474ec5da48de892aea0774cf32e41db4208
unknown <admin@IT1.colordigital.local>
2024-11-19 15:25:37 -0600

    Config api to SPA

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactRequest;
 use App\Models\Contact;
 use Illuminate\Http\Request;
 
@@ -16,7 +17,7 @@ class ContactController extends Controller
         ]);
     }
 
-    public function store(Request $request)
+    public function store(ContactRequest $request)
     {
         $data = Contact::create([
             'name' => $request->name,
